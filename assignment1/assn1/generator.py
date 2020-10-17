2663685e322048f40a1aca30376a6f83b212b474
jyroy <jyroooy@163.com>
2020-10-17 21:54:08 +0800

    update assn1 report

--- assignment1/assn1/generator.py
+++ assignment1/assn1/generator.py
@@ -2,6 +2,8 @@
 # -*- coding:utf-8 -*-
 # Author: z5304897 Junyu Zhang
 import random
+from random import choice
+
 
 '''
 I am going to generate 13 cnf files automatically for this question.
@@ -10,33 +12,31 @@ So you just need to run this program one time and input the parameters by follow
 
 if __name__ == '__main__':
     # generate 13 files for test
-    for count in range(0,13):
-        # create a cnf file
+    for count in range(15,16):
+        # create a cnfW file
         filename = "file" + str(count) + ".cnf"
         fo = open(filename, "w")
 
-        print("Start {}: The current file is {} !!!!!!!!".format(count, filename))
+        print("Start %d: The current file is %s !!!!!!!!" %(count, filename))
         # input the number of propositional variables and clauses
         variables = input("The number of propositional variables:")
         clauses = input("The number of clauses:")
 
         # write the first two line into the current file
-        fo.write("c example CNF file with {} propositional variables and {} clauses\n".format(variables, clauses))
-        fo.write("p cnf {} {}\n".format(variables, clauses))
+        fo.write("c example CNF file with %s propositional variables and %s clauses\n" %(variables, clauses))
+        fo.write("p cnf %s %s\n" %(variables, clauses))
         variables = int(variables)
 
         # generate variables and clauses randomly
         for i in range(int(clauses)):
             clause = ""
+            temp_int = []
             for j in range(3):
-                temp_int = random.randint(-variables, variables)
-                while(temp_int == 0):
-                    temp_int = random.randint(-variables, variables)
-                if str(temp_int) and str(-temp_int) not in clause:
-                    clause += str(temp_int) + " "
+                temp_int.append(choice([k for k in range(-variables, variables) if k not in temp_int and k != 0 and -k not in temp_int]))
+                clause += str(temp_int[j]) + " "
             clause += "0\n"
             fo.write(clause)
 
         fo.close()
 
-        print("Finish {}: The {} file is generated !!!!!!!!".format(count, filename))
+        print("Finish %d: The %s file is generated !!!!!!!!" %(count, filename))
